831098ab108047756af3064ef8cfe7204016111d
DGideonTech <110066917+DGideonTech@users.noreply.github.com>
2023-02-12 02:33:36 +0100

    Add files via upload
    new commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const reg = document.querySelector('.reg');
+const key = document.querySelector('.dkey');
+key.addEventListener('click', function(e){
+   reg.classList.toggle('show');
+})
+const bod = document.querySelector('.body');
+
+/* window.addEventListener('mouseover', function(e){
+/* console.log(e.prevent);
+console.log(e.target);
+}) */
